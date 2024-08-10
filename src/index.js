6a187baeca696ee0302cf4844d70df9009b7eb8f
stenyukovs <stenyukovs@gmail.com>
2024-08-10 22:24:08 +0300

    solution:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let map = new Map();
+  for (let i = 0; i <bracketsConfig.length; i++){
+    map.set(bracketsConfig[i][0], 0);
+    map.set(bracketsConfig[i][1], 0);
+  }
+
+  var splitted = str.split('');
+  for (let j = 0; j <splitted.length; j++){
+  if (map.has(splitted[j])){
+    map.set(splitted[j],   map.get(splitted[j]) +1 );
+  }
+  }
+  for (let s = 0; s <bracketsConfig.length; s++){
+    if (map.get(bracketsConfig[s][0]) != map.get(bracketsConfig[s][1]))
+      {
+      console.log('before false');
+       return false;}
+  }
+  console.log('before true');
+  return true;
 }
